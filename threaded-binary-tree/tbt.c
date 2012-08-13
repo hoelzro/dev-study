fb1abd709847c93e0fe87252d31dd177a9947dd3
Rob Hoelz <rob@hoelz.ro>
2012-08-13 21:35:57 +0200

    Fix threading when deleting

--- threaded-binary-tree/tbt.c
+++ threaded-binary-tree/tbt.c
@@ -115,27 +115,56 @@ _find_rightmost_node(struct tbt_node *node, struct tbt_node **rightmost_parent)
 }
 
 static void
-_perform_node_delete(struct tbt_node *node, struct tbt_node **node_source)
+_fix_threading(struct tbt_node *node, struct tbt_node *parent, struct tbt_node **start)
+{
+    struct tbt_node **previous = NULL;
+
+    if(node->left) {
+        struct tbt_node *throwaway = NULL;
+        struct tbt_node *rightmost = NULL;
+
+        rightmost = _find_rightmost_node(node->left, &throwaway);
+        previous  = &(rightmost->next);
+    } else {
+        if(! parent || parent->left == node) {
+            previous = start;
+        } else { /* is right child */
+            previous = &(parent->next);
+        }
+    }
+
+    *previous = node->next;
+}
+
+static void
+_perform_node_delete(struct tbt_node *node, struct tbt_node *parent, struct tbt_node **start, struct tbt_node **node_source)
 {
     if(node->left && node->right) {
         struct tbt_node *rightmost_parent = node;
         struct tbt_node *rightmost        = _find_rightmost_node(node->left,
             &rightmost_parent);
+
         node->value = rightmost->value;
 
         if(rightmost_parent == node) {
-            _perform_node_delete(rightmost, &(rightmost_parent->left));
+            _perform_node_delete(rightmost, rightmost_parent,
+                start, &(rightmost_parent->left));
         } else {
-            _perform_node_delete(rightmost, &(rightmost_parent->right));
+            _perform_node_delete(rightmost, rightmost_parent,
+                &(rightmost_parent->next), &(rightmost_parent->right));
         }
     } else if(node->left || node->right) {
         struct tbt_node *child = node->left
             ? node->left
             : node->right;
 
+        _fix_threading(node, parent, start);
+
         *node_source = child;
         free(node);
     } else { /* no children */
+        _fix_threading(node, parent, start);
+
         *node_source = NULL;
         free(node);
     }
@@ -144,6 +173,7 @@ _perform_node_delete(struct tbt_node *node, struct tbt_node **node_source)
 void
 tbt_delete(struct tbt *tree, int value)
 {
+    struct tbt_node **start = &(tree->start);
     struct tbt_node *parent = NULL;
     struct tbt_node *node   = tree->root;
 
@@ -161,7 +191,7 @@ tbt_delete(struct tbt *tree, int value)
                 node_source = &(tree->root);
             }
 
-            _perform_node_delete(node, node_source);
+            _perform_node_delete(node, parent, start, node_source);
             break;
         } else if(value < node_value) {
             if(node->left) {
@@ -172,6 +202,7 @@ tbt_delete(struct tbt *tree, int value)
             }
         } else {
             if(node->right) {
+                start  = &(node->next);
                 parent = node;
                 node   = node->right;
             } else {
